b5015437fe76edad07f067927378b4343b125beb
Thomas J Kane <thomasjkane@optonline.net>
2019-04-24 22:47:53 -0400

    commit

--- BlotPlotter.js
+++ BlotPlotter.js
@@ -1,17 +1,21 @@
-// 
-  var BlotPlotter;
+
+'''  var BlotPlotter;
 
   function preload() {
     BlotPlotter = loadJSON(BlotPlotter.json);
   }
 
-
+'''
   function setup() {
     createCanvas(400, 400);
   }
 
   function draw() {
-    bacgrown(0);
+    background(0);
+    fill(255, 0, 0);
+    rect(100, 100, 50, 50);
+    '''
     fill(BlotPlotter.red, BlotPlotter.green, BlotPlotter.blue);
     test(BlotPlotter.hostname, 10 50);
+    '''
   }
